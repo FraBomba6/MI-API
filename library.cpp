6dc03dd48231d3c1d34bb48e640b60a20edcdc0b
FraBomba6 <bombasseidebona.francesco@spes.uniud.it>
2023-01-29 15:17:52 +0100

    feat: constructor, t_n, data preprocessing

--- library.cpp
+++ library.cpp
@@ -1,10 +1,53 @@
 #include "library.h"
 #include "mlpack/core.hpp"
 
+using namespace arma;
+using namespace std;
+
 #include <iostream>
 
 void hello() {
     std::cout << "Hello, World!" << std::endl;
 }
 
+GKOVEstimator::GKOVEstimator(double (*callback)(int)) {
+    t_n_ = callback;
+}
 
+double GKOVEstimator::t_n(int n) {
+    return t_n_(n);
+}
+
+/*
+ * Prepare data for estimation.
+ * @param X - X values
+ * @param Y - Y values
+ * @param sizeOfX - size of X
+ * @param sizeOfY - size of Y
+ * @return data for estimation as arma::mat
+ */
+mat GKOVEstimator::prepare_data(double* X, double** Y, int sizeOfX, pair<int, int> sizeOfY) {
+    check_dimensions(sizeOfX, sizeOfY);
+    mat data(sizeOfX, sizeOfY.second + 1);
+    data.col(0) = vec(X, sizeOfX);
+    for (int i = 0; i < sizeOfY.second; i++) {
+        data.col(i + 1) = vec(Y[i], sizeOfX);
+    }
+    return data;
+}
+
+/*
+ * Check if dimensions are correct.
+ * @param sizeOfX - size of X
+ * @param sizeOfY - size of Y
+ * @return true if dimensions are correct, otherwise raise an exception
+ */
+bool GKOVEstimator::check_dimensions(int sizeOfX,  pair<int, int> sizeOfY) {
+    if (sizeOfX <= 0) {
+        throw std::invalid_argument("Size of X must be greater than 0.");
+    }
+    if (sizeOfY <= 0) {
+        throw std::invalid_argument("Size of Y must be greater than 0.");
+    }
+    return sizeOfX > 0 && sizeOfY > 0;
+}
--- library.h
+++ library.h
@@ -1,8 +1,21 @@
 #ifndef SMALL_PROJECT_LIBRARY_H
 #define SMALL_PROJECT_LIBRARY_H
 
+#include <armadillo>
+
+using namespace arma;
+using namespace std;
+
 void hello();
 
+class GKOVEstimator {
+    public:
+        explicit GKOVEstimator(double (*callback)(int));
+    private:
+        double (*t_n_)(int);
+        double t_n(int n);
+        static mat prepare_data(double* X, double** Y, int sizeOfX, pair<int, int> sizeOfY);
+        static bool check_dimensions(int sizeOfX, pair<int, int> sizeOfY);
+};
 
-
-#endif //SMALL_PROJECT_LIBRARY_H
+#endif
